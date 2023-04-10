74edfdd0546589e2a05bff6665b5ae4ead6be3c0
NeroKazama <ahmadkazama77@gmail.com>
2023-04-11 02:11:18 +0330

    Socket app

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 let app = require('express')();
-
 let http = require('http').Server(app);
-
-let io = require('socket.io')(http);
+let io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-io.on('connection', (socket) => {
-    io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+http.listen(3000, () => {
+    console.log('Connected')
+})
 
+io.on('connection', (socket) => {
+
+    io.emit('connections', Object.keys(io.sockets.connected).length)
 
     socket.on('disconnect', () => {
         console.log('disconnected')
-        io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
     })
 
-
-
-    socket.on('chat-message', (msg) => {
-        socket.broadcast.emit('chat-message', msg)
+    socket.on('Created', (data) => {
+        socket.broadcast.emit('Created', (data))
     })
-    socket.on('joined', (name) => {
-        socket.broadcast.emit('joined', name)
-    })
-    socket.on('leaved', (name) => {
-        socket.broadcast.emit('leaved', name)
+
+    socket.on('chat-message', (data) => {
+        socket.broadcast.emit('chat-message', (data))
     })
 
     socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data)
-    })
-    socket.on('stoptyping', () => {
-        socket.broadcast.emit('stoptyping')
+        socket.broadcast.emit('typing', (data))
     })
 
+    socket.on('stopTyping', (data) => {
+        socket.broadcast.emit('stopTyping', (data))
+    })
 
-})
+    socket.on('joined', (data) => {
+        socket.broadcast.emit('joined', (data))
+    })
 
-http.listen(3000, () => {
-    console.log('Server is started at http://localhost:3000')
+    socket.on('leaved', (data) => {
+        socket.broadcast.emit('leaved', (data))
+    })
 })
